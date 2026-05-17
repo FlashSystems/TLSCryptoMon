51954379dcb705b1ecf33c4e590427af0bd330b6
Daniel Goß <developer@flashsystems.de>
2026-05-17 16:27:11 +0200

    Make the code more modular to allow better testing
    Currently eBPF programs of type BPF_PROG_TYPE_SK_MSG can not be tested
    via the BPF_PROG_RUN command.
    The changes in this commit break up the code to make it more testable as
    soon as the kernel and libbpf allow testing BPF_PROG_TYPE_SK_MSG
    programs.

--- src/ebpf.rs
+++ src/ebpf.rs
@@ -1,6 +1,28 @@
 // SPDX-License-Identifier: BSD-3-Clause
-use std::{io::{Cursor, Read}, net::{IpAddr}};
+use std::{fs::File, io::{Cursor, Read}, net::IpAddr, os::fd::AsRawFd, os::fd::AsFd, time::Duration};
 use byteorder::{NativeEndian, ReadBytesExt};
+use libbpf_rs::{Link, RingBuffer, skel::SkelBuilder as _};
+use libbpf_rs::skel::OpenSkel as _;
+use libbpf_rs::MapCore;
+use libbpf_rs::MapFlags;
+use thiserror::Error;
+use log::{debug, info, warn};
+
+// Import the eBPF program that the build script prepared.
+mod tracecon {
+	include!(concat!(env!("OUT_DIR"), "/tlshstrace.skel.rs"));
+}
+use tracecon::*;
+
+#[derive(Error, Debug)]
+pub enum Error {
+	#[error("Error while loding eBPF program into kernel: {0}")]
+	Ebpf(#[from]libbpf_rs::Error),
+	#[error("Could not increase rlimit")]
+	MemlockRlimit,
+	#[error("Broken eBPF program. Section 'bss_data' missing.")]
+	NoBssData
+}
 
 /// Data from eBPF output structure
 #[derive(Debug)]
@@ -18,9 +40,11 @@ pub struct EbpfOutput {
 const AF_INET: u32= 2;
 const AF_INET6: u32 = 10;
 
-/// Read an ipv4/ipv6 address from the data package.
-/// The af (address family) value must be set to the address family from the
-/// output data packet.
+/**
+ * Read an ipv4/ipv6 address from the data package.
+ * The af (address family) value must be set to the address family from the
+ * output data packet.
+ */
 fn read_address(src: &mut impl Read, af: u32) -> Result<IpAddr, std::io::Error> {
     let mut address = [0u8; 16];
     src.read_exact(&mut address)?;
@@ -39,6 +63,9 @@ fn read_address(src: &mut impl Read, af: u32) -> Result<IpAddr, std::io::Error> 
     }
 }
 
+/**
+ * Unserialize the result struct of the eBPF program. From a u8 array.
+ */
 impl TryFrom<&[u8]> for EbpfOutput {
     type Error = std::io::Error;
 
@@ -58,4 +85,181 @@ impl TryFrom<&[u8]> for EbpfOutput {
             named_group: data.read_u16::<NativeEndian>()?
         })
     }
+}
+
+pub type MonitoredCGroup = Link;
+
+/**
+ * Represents an opened eBPF program that was not loaded.
+ */
+pub struct Ebpf<'obj> {
+    open_skel: OpenTlshstraceSkel<'obj>
+}
+
+impl<'obj> Ebpf<'obj> {
+    // Result codes for the callback passed to init.
+    pub const CALLBACK_OK: i32 = 0;
+    #[allow(unused)]
+    pub const CALLBACK_FAILED: i32 = -1;
+
+    /**
+     * Initializes the tlshstrace eBPF program.
+     * The debug_bpf parameter enables debug logging from the eBPF program.
+     * If this is enabled in_const_debug will be set to 1 by manipulating the
+     * const value before the program is optimized by the kernel.
+     */
+    pub fn new(object: &'obj mut std::mem::MaybeUninit<libbpf_rs::OpenObject>, debug_ebpf: bool) -> Result<Self, Error> {
+        let tls_hs_trace_skel = TlshstraceSkelBuilder::default();
+
+        let mut open_skel = tls_hs_trace_skel.open(object)?;
+
+        // Initialize the in_const_debug variable to enable or disable ebpf debugging.
+        if let Some(ref mut bss_data) = open_skel.maps.bss_data {
+            bss_data.in_const_debug = if debug_ebpf {
+                info!("Enabling debug output for eBPF program.");
+                1
+            } else {
+                0
+            };
+        } else {
+            return Err(Error::NoBssData);
+        };
+
+        Ok(Self{
+            open_skel
+        })
+    }
+
+    /**
+     * Initializes the eBPF program and sets up the output ringbuffer.
+     * This will also call the eBPF verifier.
+     */
+    pub fn init<F>(mut self, ports: &[u16], callback: F) -> Result<ConfiguredEbpf<'obj>, Error>
+        where F: FnMut(&[u8]) -> i32 + 'obj {
+
+        // Allocate the in_ports map to fit the necessary ports into it.
+        debug!("Reserve {} slots for ports in 'in_ports' map.", ports.len());
+        self.open_skel.maps.in_ports.set_max_entries(ports.len() as u32)?;
+
+        // Show the size of the output ring buffer.
+        debug!("Output ring buffer size is {} kBytes.", self.open_skel.maps.output.max_entries() / 1024);
+
+        // Load the eBPF program after we modified the configuration.
+        debug!("Loading eBPF program...");
+        let skel = self.open_skel.load()?;
+
+        // Initialize the ports list.
+        debug!("Updating ports list...");
+        let dummy = [1u8];
+        for port in ports {
+            debug!("Add port {port} to the 'in_ports' map.");
+            skel.maps.in_ports.update(&port.to_ne_bytes(), &dummy, MapFlags::ANY)?;
+        }
+
+	    // Attach the ringbuffer for data transfer between the eBPF program and userspace
+    	debug!("Attaching ringbuffer...");
+
+        let mut output_buffer_builder = libbpf_rs::RingBufferBuilder::new();
+        output_buffer_builder.add(&skel.maps.output, callback)?;
+        let output_buffer = output_buffer_builder.build()?;
+
+        Ok(ConfiguredEbpf{
+            skel,
+            output_buffer
+        })
+    }
+}
+
+/**
+ * Represents an eBPF program that is loaded into the kernel.
+ */
+pub struct ConfiguredEbpf<'obj> {
+    skel: TlshstraceSkel<'obj>,
+    output_buffer: RingBuffer<'obj>
+}
+
+impl<'obj> ConfiguredEbpf<'obj> {
+    /**
+     * Called to process the ringbuffer. If the ringbuffer is empty this call
+     * will wait 250ms before returning.
+     * It will also return if an Interrupted-Error occurs. This error is not
+     * passed back to the caller.
+     */
+    pub fn process_output_buffer(&self) -> Result<(), Error> {
+        match self.output_buffer.poll(Duration::from_millis(250)) {
+            Ok(_) => Ok(()),
+            // Ignore the Interrupted error. Just return ok. The caller will call us again if neccessary.
+            Err(err) if err.kind() == libbpf_rs::ErrorKind::Interrupted  => Ok(()),
+            // Return all other values unaltered
+            Err(err) => Err(Error::from(err))
+        }
+    }
+
+    /**
+     * Attaches the `bpf_socket_operation` (BPF_PROG_TYPE_SOCK_OPS) eBPF function
+     * to a cGroup given cgroup. This makes sure the function is called at specific
+     * lifecycle events of a socket.
+     * It also attaches the `bpf_stream_parser` to the `tls_sockets` sockmap.
+     * This sockmap is that is filled by `bpf_socket_operation`. This makes sure
+     * that traffic for each socket that is put into the `tls_sockets` map by
+     * `bpf_socket_operation` is processed by the stream parser.
+     * If this function is not called, nothing will be processed by the eBPF
+     * program.
+     */
+    pub fn attach_to_cgroup(&mut self, cgroup: File) -> Result<MonitoredCGroup, Error> {
+        // Attach the eBPF program to the selected cgroup.
+        debug!("Attaching eBPF program to cgroup FD...");
+        let cgroup_fd = cgroup.as_fd();
+        let cgroup_link = self.skel.progs.bpf_socket_operation.attach_cgroup(cgroup_fd.as_raw_fd())?;
+
+        // Attach the other eBPF program to the socket_map. The first program adds every socket that fits
+        // the port filter to this socket map and makes the bpf_stream_parser run.
+        debug!("Attaching eBPF program to socket map 'tls_sockets'...");
+        let tls_socketmap_fd = self.skel.maps.tls_sockets.as_fd();
+        self.skel.progs.bpf_stream_parser.attach_sockmap(tls_socketmap_fd.as_raw_fd())?;
+
+        Ok(cgroup_link as MonitoredCGroup)
+    }
+}
+
+/**
+ * This function is passed to libbpf_rs::set_print to funnel
+ * all eBPF output through this library.
+ */
+fn libbpf_print(level: libbpf_rs::PrintLevel, msg: String) {
+    match level {
+        libbpf_rs::PrintLevel::Debug => debug!("{msg}"),
+        libbpf_rs::PrintLevel::Info => info!("{msg}"),
+        libbpf_rs::PrintLevel::Warn => warn!("{msg}")
+    }
+}
+
+/**
+ * Initializes log forwarding for libebpf based on the loglevel based on
+ * `log::LevelFilter`.
+ */
+pub fn init_logging(level: log::LevelFilter) {
+    let libbpf_print_level = match level {
+        log::LevelFilter::Off | log::LevelFilter::Error | log::LevelFilter::Warn => libbpf_rs::PrintLevel::Warn,
+        log::LevelFilter::Info => libbpf_rs::PrintLevel::Info,
+        log::LevelFilter::Debug | log::LevelFilter::Trace => libbpf_rs::PrintLevel::Debug
+    };
+
+    libbpf_rs::set_print(Some((libbpf_print_level, libbpf_print)));
+}
+
+/**
+ * Increase the memlock limit to allow the creation of all necessary BPF maps.
+ */
+pub fn bump_memlock_rlimit() -> Result<(), Error> {
+	let rlimit = libc::rlimit {
+		rlim_cur: 128 << 20,
+		rlim_max: 128 << 20,
+	};
+
+	if unsafe { libc::setrlimit(libc::RLIMIT_MEMLOCK, &rlimit) } == 0 {
+		Ok(())
+	} else {
+		Err(Error::MemlockRlimit)
+	}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,29 @@
 // SPDX-License-Identifier: BSD-3-Clause
 use std::mem::MaybeUninit;
-use std::os::fd::AsFd;
-use std::os::fd::AsRawFd;
 use std::sync::Arc;
 use std::sync::atomic::AtomicBool;
-use std::time::Duration;
-use libbpf_rs::MapCore;
-use libbpf_rs::MapFlags;
-use libbpf_rs::skel::SkelBuilder as _;
-use libbpf_rs::skel::OpenSkel as _;
-use log::{debug, error, warn, info};
+use log::{debug, error, warn};
 use thiserror::Error;
 use time::OffsetDateTime;
 use time::format_description::FormatItem;
 
 mod ebpf;
-use ebpf::EbpfOutput;
+use ebpf::{EbpfOutput, Ebpf};
+
 mod tls;
 mod cmdline;
 
 /// Timestamp format
 const TIMESTAMP_FORMAT: &[FormatItem] = time::macros::format_description!("[year]-[month]-[day]T[hour]:[minute]:[second].[subsecond digits:3]Z");
 
-// Import the eBPF program that the build script prepared.
-mod tracecon {
-	include!(concat!(env!("OUT_DIR"), "/tlshstrace.skel.rs"));
-}
-use tracecon::*;
-
 #[derive(Error, Debug)]
 pub enum RuntimeError {
-	#[error("Error while loding eBPF program into kernel: {0}")]
-	EbpfError(#[from]libbpf_rs::Error),
-	#[error("Could not increase rlimit")]
-	MemlockRlimit,
+	#[error("eBPF error: {0}")]
+	EbpfError(#[from]ebpf::Error),
 	#[error("Could not open cgroup: {0}")]
 	CgroupOpen(std::io::Error),
 	#[error("Broken eBPF program. Section 'bss_data' missing.")]
 	NoBssData
-}
-
-fn bump_memlock_rlimit() -> Result<(), RuntimeError> {
-	let rlimit = libc::rlimit {
-		rlim_cur: 128 << 20,
-		rlim_max: 128 << 20,
-	};
-
-	if unsafe { libc::setrlimit(libc::RLIMIT_MEMLOCK, &rlimit) } == 0 {
-		Ok(())
-	} else {
-		Err(RuntimeError::MemlockRlimit)
-	}
 }
 
 fn process_tls_session(non_pq_only: bool, data: &[u8], ringbuffer_full_counter: &mut u64, invalid_packet_counter: &mut u64) {
@@ -92,34 +65,9 @@ fn process_tls_session(non_pq_only: bool, data: &[u8], ringbuffer_full_counter: 
 	}
 }
 
-/// This function is passed to libbpf_rs::set_print to funnel
-/// all eBPF output through this library.
-fn libbpf_print(level: libbpf_rs::PrintLevel, msg: String) {
-    match level {
-        libbpf_rs::PrintLevel::Debug => debug!("{msg}"),
-        libbpf_rs::PrintLevel::Info => info!("{msg}"),
-        libbpf_rs::PrintLevel::Warn => warn!("{msg}")
-    }
-}
-
-/// Removes errors of kind Interrupted from the passed Result. Returns Ok(default) instead.
-/// This is used to handle Ctrl+C wihtout erroring out.
-fn filter_int_error<R>(result: Result<R, libbpf_rs::Error>, default: R) -> Result<R, libbpf_rs::Error>{
-	match result {
-		Ok(_) => result,
-		Err(err) => {
-			if err.kind() == libbpf_rs::ErrorKind::Interrupted {
-				Ok(default)
-			} else {
-				Err(err)
-			}
-		}
-	}
-}
-
 fn run(config: cmdline::Config) -> Result<(), RuntimeError> {
 	debug!("Increasing memlock limit...");
-	bump_memlock_rlimit()?;
+	ebpf::bump_memlock_rlimit()?;
 
 	debug!("Initializing shutdown handler...");
 	let shutdown = Arc::new(AtomicBool::new(false));    // Gibt's da nichts besseres?
@@ -129,78 +77,34 @@ fn run(config: cmdline::Config) -> Result<(), RuntimeError> {
 		handler_shutdown.store(true, std::sync::atomic::Ordering::SeqCst);
 	}).expect("Error setting Ctrl-C handler");
 
-	let tls_hs_trace_skel = TlshstraceSkelBuilder::default();
-
-	debug!("Reading and configuring eBPF program...");
-
+	debug!("Opening eBPF program...");
 	let mut open_object = MaybeUninit::uninit();
-	let mut open_skel = tls_hs_trace_skel.open(&mut open_object)?;
+	let open_ebpf = Ebpf::new(&mut open_object, config.debug_ebpf)?;
 
-	// Allocate the in_ports map to fit the necessary ports into it.
-	debug!("Reserve {} slots for ports in 'in_ports' map.", config.ports.len());
-	open_skel.maps.in_ports.set_max_entries(config.ports.len() as u32)?;
-
-	// Show the size of the output ring buffer.
-	debug!("Output ring buffer size is {} kBytes.", open_skel.maps.output.max_entries() / 1024);
-
-	// Initialize the in_const_debug variable to enable or disable ebpf debugging.
-	if let Some(ref mut bss_data) = open_skel.maps.bss_data {
-		bss_data.in_const_debug = if config.debug_ebpf {
-			info!("Enabling debug output for eBPF program.");
-			1
-		} else {
-			0
-		};
-	} else {
-		return Err(RuntimeError::NoBssData);
-	};
-
-	// Load the eBPF program after we modified the configuration.
 	debug!("Loading eBPF program...");
-	let skel = open_skel.load()?;
-
-	// Initialize the ports list.
-	debug!("Updating ports list...");
-	let dummy = [1u8];
-	for port in config.ports {
-		debug!("Add port {port} to the 'in_ports' map.");
-		skel.maps.in_ports.update(&port.to_ne_bytes(), &dummy, MapFlags::ANY)?;
-	}
 
 	// These variables are passed to the ring buffer reader and store the last value of these variables.
 	// If the value changes, the new values are output as warnings.
 	let mut ringbuffer_full_counter = 0;
 	let mut invalid_packet_counter = 0;
-
-	// Attach the ringbuffer for data transfer between the eBPF program and userspace
-	debug!("Attaching ringbuffer...");
-    let mut output_buffer_builder = libbpf_rs::RingBufferBuilder::new();
-
 	let non_pq_only = config.non_pq_only;
-	output_buffer_builder.add(&skel.maps.output, move |data| { process_tls_session(non_pq_only, data, &mut ringbuffer_full_counter, &mut invalid_packet_counter); 0 })?;
-	let output_buffer = output_buffer_builder.build()?;
 
-	// FIXME: What cgroup should we use?
+	let mut ebpf = open_ebpf.init(
+		&config.ports,
+		move |data| { process_tls_session(non_pq_only, data, &mut ringbuffer_full_counter, &mut invalid_packet_counter); Ebpf::CALLBACK_OK }
+	)?;
+
+	debug!("Attaching to cgroup {}...", config.cgroup.to_string_lossy());
 	let cgroup = std::fs::File::open(config.cgroup).map_err(RuntimeError::CgroupOpen)?;
-
-	// Attach the eBPF program to the selected cgroup.
-	debug!("Attaching eBPF program to cgroup...");
-	let cgroup_fd = cgroup.as_fd();
-	let keep = skel.progs.bpf_socket_operation.attach_cgroup(cgroup_fd.as_raw_fd())?;
-
-	// Attach the other eBPF program to the socket_map. The first program adds every socket that fits
-	// the port filter to this socket map and makes the bpf_stream_parser run.
-	debug!("Attaching eBPF program to socket map 'tls_sockets'...");
-	let tls_socketmap_fd = skel.maps.tls_sockets.as_fd();
-	skel.progs.bpf_stream_parser.attach_sockmap(tls_socketmap_fd.as_raw_fd())?;
+	let monitored_cgroup = ebpf.attach_to_cgroup(cgroup)?;
 
 	debug!("Waiting for messages...");
 	while !shutdown.load(std::sync::atomic::Ordering::SeqCst) {
-		filter_int_error(output_buffer.poll(Duration::from_millis(250)), ())?;
+		ebpf.process_output_buffer()?;
 	}
 
 	debug!("Shutting down...");
-	drop(keep);
+	drop(monitored_cgroup);
 
 	Ok(())
 }
@@ -210,15 +114,14 @@ fn main() {
 		Ok(config) => {
 			// Initialize logging
 			let log_level = if config.debug {
-				libbpf_rs::set_print(Some((libbpf_rs::PrintLevel::Debug, libbpf_print)));
 				log::LevelFilter::Trace
 			} else if config.verbose {
-				libbpf_rs::set_print(Some((libbpf_rs::PrintLevel::Info, libbpf_print)));
 				log::LevelFilter::Info
 			} else {
-				libbpf_rs::set_print(Some((libbpf_rs::PrintLevel::Warn, libbpf_print)));
 				log::LevelFilter::Warn
 			};
+
+			ebpf::init_logging(log_level);
 
 			// Initialize the logger with the configured log level.
 			// Use the systemd journal if it is avaiable
